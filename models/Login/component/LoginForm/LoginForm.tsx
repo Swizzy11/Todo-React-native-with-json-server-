23e9c845242d25e36ad93fbe9a84ea0fcf11902c
Dmitriy Kotovskij <kotovskij.01@yandex.ru>
2023-10-30 11:25:16 +0300

    Исправил ошибку валидации данных при авторизации

--- models/Login/component/LoginForm/LoginForm.tsx
+++ models/Login/component/LoginForm/LoginForm.tsx
@@ -5,6 +5,7 @@ import { ButtonAdd } from './component/Button'
 import { router } from 'expo-router'
 import { userAPI } from '../../../../service/UserService'
 import { Text } from 'react-native'
+import { validator } from '../../../../utils/validator'
 
 const LoginForm:FC = () => {
     const [loginUser, {status}] = userAPI.useLoginMutation()
@@ -13,68 +14,73 @@ const LoginForm:FC = () => {
     const [error, setError] = useState('')
 
     const authUser = () => {
+        const validityEmail = validator('email', email)
+        const validityPassword = validator('password', password)
+
         setError('')
 
-        if(email !== '' && password !== '') {
+        if(validityEmail !== '' && validityPassword !== '') {
+            return setError('Неверный формат логина или пароля')
+        }else {
             loginUser(
                 {
                     email: email,
                     password: password,
                 }     
             )
+            
             if(status === 'fulfilled') {
                 router.push('/home')
             }else {
                 setError(`Неверный логин или пароль`)
             }
-        }else {
-            setError('Введите логин и пароль')
         }
     }
-  return (
-        <Form>
-            <FormItem>
-                <FormTitle>Авторизация</FormTitle>
-            </FormItem>
-            <FormItem>
-                <Label>Логин</Label>
-                <Input 
-                    value={email} 
-                    onChangeText={setEmail} 
-                    placeholder='email'
-                />
-            </FormItem>
-            <FormItem>
-                <Label>Пароль</Label>
-                <Input 
-                    value={password} 
-                    onChangeText={setPassword}
-                    secureTextEntry={true}
-                    placeholder='password...'
-                />
-            </FormItem>
-            <FormItem>
-                <Text style={{color: 'red'}}>
-                    {error}
-                </Text>
-            </FormItem>
-            <FormItem>
-                <ButtonAddWrapper>
-                    <ButtonAdd 
-                        onPress={() => {
-                            authUser()
-                        }}
-                        title='Войти' 
+
+    return (
+            <Form>
+                <FormItem>
+                    <FormTitle>Авторизация</FormTitle>
+                </FormItem>
+                <FormItem>
+                    <Label>Логин</Label>
+                    <Input 
+                        value={email} 
+                        onChangeText={setEmail} 
+                        placeholder='email'
                     />
-                    <Text>или</Text>
-                    <ButtonAdd 
-                        title='Зарегистрироваться' 
-                        onPress={() => router.push('/registration')} 
+                </FormItem>
+                <FormItem>
+                    <Label>Пароль</Label>
+                    <Input 
+                        value={password} 
+                        onChangeText={setPassword}
+                        secureTextEntry={true}
+                        placeholder='password...'
                     />
-                </ButtonAddWrapper>
-            </FormItem>
-        </Form>
-  )
+                </FormItem>
+                <FormItem>
+                    <Text style={{color: 'red'}}>
+                        {error}
+                    </Text>
+                </FormItem>
+                <FormItem>
+                    <ButtonAddWrapper>
+                        <ButtonAdd 
+                            onPress={() => {
+                                authUser()
+                            }}
+                            title='Войти' 
+                        />
+                        <Text>или</Text>
+                        <ButtonAdd 
+                            title='Зарегистрироваться' 
+                            onPress={() => router.push('/registration')} 
+                        />
+                    </ButtonAddWrapper>
+                </FormItem>
+            </Form>
+    )
 }
 
 
